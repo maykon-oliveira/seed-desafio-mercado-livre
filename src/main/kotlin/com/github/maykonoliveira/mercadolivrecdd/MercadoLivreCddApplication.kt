52839667b52024f672c220011f8d28373c93b513
Maykon Oliveira <C0635611@vale.com>
2022-09-02 16:12:55 -0300

    Setup do projeto

---
+++ src/main/kotlin/com/github/maykonoliveira/mercadolivrecdd/MercadoLivreCddApplication.kt
@@ -0,0 +1,11 @@
+package com.github.maykonoliveira.mercadolivrecdd
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MercadoLivreCddApplication
+
+fun main(args: Array<String>) {
+	runApplication<MercadoLivreCddApplication>(*args)
+}
---
+++ src/test/kotlin/com/github/maykonoliveira/mercadolivrecdd/MercadoLivreCddApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.github.maykonoliveira.mercadolivrecdd
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MercadoLivreCddApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
